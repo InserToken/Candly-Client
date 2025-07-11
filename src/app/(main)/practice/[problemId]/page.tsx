c239c3d3a3abeff374592feabc2a46792063c81d
곽예경 <kyun9.k@gmail.com>
2025-07-11 16:08:05 +0900

    [feat1] minor change on navbar #1

--- src/app/(main)/practice/[problemId]/page.tsx
+++ src/app/(main)/practice/[problemId]/page.tsx
@@ -1,1 +1,10 @@
+import Practice from "@/components/blocks/PracticePage";
 import React from "react";
+
+export default function PracticePage() {
+  return (
+    <div>
+      <Practice />
+    </div>
+  );
+}
