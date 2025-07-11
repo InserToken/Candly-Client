596246fc17975ad974656bd4d61f29b69715676e
jiminseon <20201020@dongduk.ac.kr>
2025-07-11 15:52:18 +0900

    [feat] 계좌연결 퍼블리싱 #10

---
+++ src/components/blocks/Investment/Investment.client.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+
+export default function InvestmentClient() {
+  return (
+    <div className="flex flex-col items-center mt-50">
+      <h1 className="text-3xl mb-4">계좌 연결하기</h1>
+      <p className="mb-8 text-xl text-center text-gray-300">
+        내 보유주식의 시세를 예측하려면 <br />
+        <span className="text-[#396FFB]">계좌 연결</span>이 필요합니다.
+      </p>
+      <div className="flex space-x-4">
+        <button className="px-14 py-4 text-2xl bg-gray-700 rounded hover:bg-gray-600">
+          뒤로가기
+        </button>
+        <button className="px-14 py-4 text-2xl bg-[#426FE5] rounded hover:bg-blue-500">
+          연결하기
+        </button>
+      </div>
+    </div>
+  );
+}
