a5aa4765f4b068f1dbb7d79348a35409d9c67c3e
sillonjeu <pius0316@naver.com>
2025-07-15 10:25:09 +0900

    [feat] Ranking.client.tsx -> 카드 글자 중앙 정렬

--- src/components/blocks/Ranking/Ranking.client.tsx
+++ src/components/blocks/Ranking/Ranking.client.tsx
@@ -6,8 +6,8 @@ export default function RankingClient() {
     <div className="flex justify-center items-center gap-10 bg-[#0f0f0f] pt-28">
       {/* 연습문제 카드 */}
       <Link href="/ranking/practice" className="group">
-        <div className="group bg-[#1C1C20] hover:bg-[#396FFB] rounded-2xl p-6 w-[450px] h-[450px] text-center  flex flex-col justify-center cursor-pointer">
-          <div className="mb-16">
+        <div className="group bg-[#1C1C20] hover:bg-[#396FFB] rounded-2xl p-6 w-[450px] h-[450px] text-center shadow-lg flex items-center justify-center cursor-pointer">
+          <div>
             <h2 className="text-[#F4F4F4] text-3xl font-semibold mb-3">
               연습문제 랭킹
             </h2>
@@ -24,8 +24,8 @@ export default function RankingClient() {
 
       {/* 실전예측 카드 */}
       <Link href="/ranking/real" className="group">
-        <div className="group bg-[#1C1C20] hover:bg-[#396FFB] rounded-2xl p-6 w-[450px] h-[450px] text-center shadow-lg flex flex-col justify-center cursor-pointer">
-          <div className="mb-16">
+        <div className="group bg-[#1C1C20] hover:bg-[#396FFB] rounded-2xl p-6 w-[450px] h-[450px] text-center shadow-lg flex items-center justify-center cursor-pointer">
+          <div>
             <h2 className="text-[#F4F4F4] text-3xl font-semibold mb-3">
               실전예측 랭킹
             </h2>
