1305f23dd804f735aa7b2248306c41fff46c91e8
sillonjeu <pius0316@naver.com>
2025-07-23 10:45:17 +0900

    [hot fix] localhost -> {process.env.NEXT_PUBLIC_API_URL} 방식으로 url 수정

--- src/services/fetchFinancial.ts
+++ src/services/fetchFinancial.ts
@@ -1,7 +1,7 @@
 export async function fetchFinancial(stockCode: string, date: string) {
   const replaceDate = date.replace(/-/g, ".");
   const res = await fetch(
-    `http://localhost:3001/api/financial/metrics?stockCode=${stockCode}&date=${replaceDate}`
+    `${process.env.NEXT_PUBLIC_API_URL}/api/financial/metrics?stockCode=${stockCode}&date=${replaceDate}`
   );
   const data = await res.json();
   return data;
